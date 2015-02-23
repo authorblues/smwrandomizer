a882894b9ffb705b98b0b14c6aa62f7fa6d089a3
Chad Waters <authorblues@gmail.com>
2015-02-23 15:39:21 -0500

    Point to correct copy of spark-md5

--- js/md5.worker.js
+++ js/md5.worker.js
@@ -1,7 +1,7 @@
 // @from http://codepen.io/tymondesigns/pen/tswhE
 self.addEventListener('message', function(e)
 {
-	importScripts('../vendor/spark-md5.min.js');
+	importScripts('vendor/spark-md5.min.js');
 	postMessage(SparkMD5.ArrayBuffer.hash(
 		(new FileReaderSync()).readAsArrayBuffer(e.data)
 	));
