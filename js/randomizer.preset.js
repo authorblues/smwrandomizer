5e3208f4747d460e877dc945f03739bf9c273ff4
Chad Waters <authorblues@gmail.com>
2015-12-29 17:50:55 -0500

    Add text options as presets

--- js/randomizer.preset.js
+++ js/randomizer.preset.js
@@ -36,6 +36,10 @@ function updatePreset(val)
 	$('#randomize_koopakids').prop('checked', preset > 3);
 	$('#randomize_bossdiff').prop('checked', preset > 5);
 	$('#remove_autoscrollers').prop('checked', preset > 4);
+	
+	$('#customnames').prop('checked', preset > 2);
+	$('#customtext').prop('checked', preset > 3);
+	$('#pogyo_mode').prop('checked', preset > 5);
 }
 
 $('#preset').change(updatePreset);
